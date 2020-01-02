87c8b9dbd50d7d057c7b0eda4e7dc1577a612246
dannysindra <als478@cornell.edu>
2020-01-01 16:08:45 -0800

    try deploy4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { ApolloServer } = require('apollo-server-cloud-functions');
+const functions = require('firebase-functions');
 const { config } = require('./config');
 
 const server = new ApolloServer(config);
 const handler = server.createHandler();
 
 module.exports = {
-    handler: server.createHandler()
+    handler: functions.https.onRequest(handler)
 };
